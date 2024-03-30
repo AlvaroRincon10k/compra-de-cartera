c0221416684ca029c3e79499fc4268e396eef522
Alvaro Rincon <alvarorinconc1@gmail.com>
2024-03-29 23:18:31 -0500

    AlertDialog.
    - Se agrega un "AlertDialog" en el "onBackPressed"  de la pantalla principal.
    - Se le asigna un color a los botones "si" y "no" del AlertDialog.

--- Compradecartera/app/src/main/java/com/example/compradecartera/presentation/ui/dashboard/DashBoardActivity.kt
+++ Compradecartera/app/src/main/java/com/example/compradecartera/presentation/ui/dashboard/DashBoardActivity.kt
@@ -1,9 +1,13 @@
 package com.example.compradecartera.presentation.ui.dashboard
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.compradecartera.R
 import com.example.compradecartera.databinding.ActivityMainBinding
 import com.example.compradecartera.di.ViewModelFactory
 import com.example.compradecartera.presentation.ui.cardnumber.CardNumber
@@ -14,7 +18,7 @@ class DashBoardActivity : AppCompatActivity() {
 
     private var amount: Double = 0.0
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: DashBoardViewModel by viewModels{
+    private val viewModel: DashBoardViewModel by viewModels {
         ViewModelFactory()
     }
 
@@ -30,7 +34,7 @@ class DashBoardActivity : AppCompatActivity() {
     }
 
     private fun initObserver() {
-        viewModel.userLiveData.observe(this) { user->
+        viewModel.userLiveData.observe(this) { user ->
             amount = user.amount
             binding.textViewUserName.text = user.name
             binding.textViewBalance.text = amount.toString()
@@ -42,5 +46,32 @@ class DashBoardActivity : AppCompatActivity() {
         val intent = Intent(this, CardNumber::class.java)
         intent.putExtra("AMOUNT", amount)
         startActivity(intent)
+    }
+
+    override fun onBackPressed() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Salir de la aplicación")
+        builder.setMessage("¿Estás seguro de que quieres salir?")
+        builder.setPositiveButton("Sí") { dialog, which ->
+            // Cierra la actividad y sale de la aplicación
+            super.onBackPressed()
+        }
+        builder.setNegativeButton("No") { dialog, which ->
+            // Cancela el diálogo y no realiza ninguna acción
+            dialog.dismiss()
+        }
+
+        val dialog = builder.create()
+
+        // Cambiar el color a los botones "si" y "no"
+        dialog.setOnShowListener { dialog ->
+            val positiveButton = (dialog as AlertDialog).getButton(DialogInterface.BUTTON_POSITIVE)
+            val negativeButton = (dialog as AlertDialog).getButton(DialogInterface.BUTTON_NEGATIVE)
+
+            positiveButton.setTextColor(ContextCompat.getColor(this, R.color.background_color))
+            negativeButton.setTextColor(ContextCompat.getColor(this, R.color.background_color))
+        }
+
+        dialog.show()
     }
 }
